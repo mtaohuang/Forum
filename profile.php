4fa2520afd03f9baf175b5d6ec0c7f496a637554
morrieati <morrislew@outlook.com>
2015-03-05 09:21:21 +0800

    创建个人资料页
    刘纪安的代码写得有些粗糙，我来润色一下。以后的前端风格可能也会要是这种纵向的了

---
+++ profile.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="UTF-8" />
+		<link rel="stylesheet" type="text/css" href="style/profile.css" />
+		<script type="text/javascript" src="script/main.js"></script>
+
+		<title>个人资料</title>
+	</head>
+	<body>
+		<div id="container">
+			<div >
+				
+			</div>
+		</div>
+	</body>
+</html>
