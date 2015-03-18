26ec35cc50f389cb0c9db03e884953bb04fec7d3
iyummy <vyummy@163.com>
2015-03-18 20:05:23 +0800

    个人页面
    晚上写的个人页面，欢迎指点~

---
+++ script/selfInfo.js
@@ -0,0 +1,1 @@
+$("#button1,#button2,#button3,#button4").button();
---
+++ selfInfo.php
@@ -0,0 +1,25 @@
+<?php
+session_start();
+?>
+<!doctype html>
+<head>
+    <meta charset="utf-8">
+    <link rel="stylesheet" style="text/css" href="style/selfInfo.css"/>
+    <link rel=stylesheet type=text/css href="style/jquery-ui.min.css"/>
+    
+    <title>个人中心</title>
+</head>
+<body>
+    <img src="image/self_background.jpg"width="100%" height="250px"/>
+    <img  id="head" src="data/head/3.jpg"/>
+    <div id="left_nav" >
+        <span id="button1" class="left_button">我的文章</span></br>
+        <span id="button2"class="left_button">与我相关</span></br>
+        <span id="button3"class="left_button">个人资料</span></br>
+        <span id="button4"class="left_button">设置</span></br>
+    </div>
+    <script src="script/jquery.js"></script>
+    <script src="script/jquery-ui.min.js"></script>
+    <script src="script/selfInfo.js"></script>
+</body>
+
