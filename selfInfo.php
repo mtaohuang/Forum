58762cedf7df8046c01c523cc46160a5e18af682
iyummy <vyummy@163.com>
2015-03-19 15:09:48 +0800

    数据库配置表和个人页面

--- selfInfo.php
+++ selfInfo.php
@@ -10,13 +10,16 @@ session_start();
     <title>个人中心</title>
 </head>
 <body>
-    <img src="image/self_background.jpg"width="100%" height="250px"/>
+    <img src="image/self_background.jpg" id="top_background"/>
     <img  id="head" src="data/head/3.jpg"/>
     <div id="left_nav" >
         <span id="button1" class="left_button">我的文章</span></br>
         <span id="button2"class="left_button">与我相关</span></br>
         <span id="button3"class="left_button">个人资料</span></br>
-        <span id="button4"class="left_button">设置</span></br>
+        <span id="button4"class="left_button">设&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;置</span></br>
+    </div>
+    <div id="right_block">
+        
     </div>
     <script src="script/jquery.js"></script>
     <script src="script/jquery-ui.min.js"></script>
