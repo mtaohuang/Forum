a4bd518bb87f1014a045bcc65298132027d997f0
刘义盟 <morrislew@outlook.com>
2015-03-25 17:23:50 +0800

    club.html页面的新增加
    在club.html中增加了导航和搜索。

---
+++ script/club.js
@@ -0,0 +1,14 @@
+window.onload = function(){
+    window.onscroll = function(){
+        var isTop = document.documentElement.scrollTop || document.body.scrollTop;
+        if (isTop < 400) {
+            document.getElementById("mid_nav").className = 'mov_nav';
+            document.getElementById("search").style.display = 'none';
+        }
+        else {
+            document.getElementById("mid_nav").className = 'fix_nav';
+            document.getElementById("search").style.display = 'block';
+        }
+        console.log(isTop);
+    }
+}
